65f9b7b5bfaeba97f0222a3ab3c6c4bb592ab985
phortonssf <phorton@ssf.com>
2018-04-28 07:44:17 -0700

    lambda test

--- functions/random.js
+++ functions/random.js
@@ -1,23 +1,28 @@
+// exports.handler = function(event, context, callback) {
+//     const randomWords = [
+//         'copper',
+//         'explain',
+//         'tenuous',
+//         'neat',
+//         'discovery',
+//         'sweltering',
+//         'dusty',
+//         'unruly',
+//         'week',
+//         'rejoice',
+//     ];
+//     const randomNumber = Math.floor(Math.random() * 10);
+//     callback(null, {
+//         statusCode: 200,
+//         headers: {
+//             'Access-Control-Allow-Origin': '*',
+//         },
+//         body: JSON.stringify({ word: randomWords[randomNumber] }),
+//     });
+// };
 exports.handler = function(event, context, callback) {
-    console.log("Hello")
-    const randomWords = [
-        'copper',
-        'explain',
-        'tenuous',
-        'neat',
-        'discovery',
-        'sweltering',
-        'dusty',
-        'unruly',
-        'week',
-        'rejoice',
-    ];
-    const randomNumber = Math.floor(Math.random() * 10);
     callback(null, {
         statusCode: 200,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
-        body: "word",
+        body: "Hello, World"
     });
-};
+}
