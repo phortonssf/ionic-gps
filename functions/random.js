6b6793bfea5f5a85582e23ee26c92f9080a45c35
phortonssf <phorton@ssf.com>
2018-04-28 10:23:09 -0700

    wtf

--- functions/random.js
+++ functions/random.js
@@ -23,6 +23,6 @@
 exports.handler = function(event, context, callback) {
     callback(null, {
         statusCode: 200,
-        body: JSON.stringify("Hello, World")
+        body: JSON.stringify({ name: "Hello, World" })
     });
 }
